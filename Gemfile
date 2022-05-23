b82a71afd6b947d86d2e8639f3e97e33a1c360b9
Will Anderson <will.anderson3401@gmail.com>
2022-05-23 13:32:25 -0500

    Built everything

--- Gemfile
+++ Gemfile
@@ -34,7 +34,7 @@ gem "redis", "~> 4.0"
 # gem "kredis"
 
 # Use Active Model has_secure_password [https://guides.rubyonrails.org/active_model_basics.html#securepassword]
-# gem "bcrypt", "~> 3.1.7"
+gem "bcrypt", "~> 3.1.7"
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem "tzinfo-data", platforms: %i[ mingw mswin x64_mingw jruby ]
@@ -70,3 +70,10 @@ group :test do
   gem "selenium-webdriver"
   gem "webdrivers"
 end
+
+gem "omniauth-twitter", "~> 1.4"
+gem "omniauth-rails_csrf_protection", "~> 1.0"
+
+gem "twitter", "~> 7.0"
+
+gem "sidekiq", "~> 6.4"
---
+++ app/controllers/password_resets_controller.rb
@@ -0,0 +1,37 @@
+class PasswordResetsController < ApplicationController
+    def new
+        
+    end
+
+    def create
+        @user = User.find_by(email: params[:email])
+
+        if @user.present?
+            # send email
+            PasswordMailer.with(user: @user).reset.deliver_now
+        end
+        redirect_to root_path, notice: "If an account with that email was found, we have sent a link to reset your password"
+    end
+
+    def edit
+        @user = User.find_signed(params[:token], purpose: "password_reset")
+    rescue ActiveSuppourt::MessageVerifier::InvalidSignature
+        redirect_to password_reset_path, alert: "Your token has expired"
+
+    end
+
+    def update
+        @user = User.find_signed(params[:token], purpose: "password_reset")
+        if @user.update(password_params)
+            redirect_to sign_in_path, notice: "Password updated!"
+        else
+            render :edit
+        end
+    end
+
+
+    def password_params
+        params.require(:user).permit(:password, :password_confirmation)
+    end
+
+end
